67d3400820b8b03d7ce88b7b9670cba19ce2b0ba
Josser <133165583+IngenieroJosser@users.noreply.github.com>
2025-01-22 22:28:23 -0500

    (styles): algunos mas retoques en el componente

--- src/page/InventoryPage.tsx
+++ src/page/InventoryPage.tsx
@@ -1,12 +1,18 @@
 import '../styles/page/inventory.css'
 import HeaderInventory from '../components/inventory/HeaderInventory'
 import MainInventory from '../components/inventory/MainInventory'
+import '../styles/page/inventory.css'
 
 const InventoryPage = () => {
   return (
     <>
       <HeaderInventory />
       <MainInventory />
+      {/* <footer className="footer">
+        <div className="footer-content">
+          <p className="footer-text">Bienvenido al Sistema de Inventario - Administra tus productos con facilidad</p>
+        </div>
+      </footer> */}
     </>
   )
 }
